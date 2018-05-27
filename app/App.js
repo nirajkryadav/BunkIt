854da710c857b1b5b4891959d402e3227aa39b33
nkyadav4598 <nirajkumar4598@gmail.com>
2018-05-27 23:06:28 +0530

    creating Components

---
+++ app/App.js
@@ -0,0 +1,39 @@
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {FbButton} from './components/FbButton'
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <FbButton/>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ app/components/FbButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const FbButton = () => {
+	return (
+		<TouchableOpacity>
+			<Text> Login</Text>
+		</TouchableOpacity>
+		)
+}
+
+export {FbButton};
