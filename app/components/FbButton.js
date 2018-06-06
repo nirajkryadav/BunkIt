4c5a45cd0b6a3de23fac947b9f0c4cc6ecc68e3c
nkyadav4598 <nirajkumar4598@gmail.com>
2018-06-06 15:01:02 +0530

    button on press

--- app/components/FbButton.js
+++ app/components/FbButton.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Button } from 'react-native'
+import { Button, Alert } from 'react-native'
 
 const FbButton = (props) => {
 	return (
 		<Button
-  onPress={'Press'}
+  onPress={() => {
+    Alert.alert('You tapped the button!');
+  }}
   title="Learn More"
   color="#0D0D0D"
   accessibilityLabel="Learn more about this purple button"/>
