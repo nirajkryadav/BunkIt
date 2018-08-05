77356cda3537f847ffbb932ad7e0727d3940782f
thehackersarena <nkrajkumar4598@gmail.com>
2018-08-05 18:29:03 +0530

    login flow

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-import { AppRegistry } from 'react-native';
-import  Basic  from './app/containers/Basic';
+import React from "react";
+import { createRootNavigator } from "./app/containers/router";
+import { isSignedIn } from "./app/containers/screens/auth";
 
-AppRegistry.registerComponent('BunkIt', () => Basic);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false,
+      checkedSignIn: false
+    };
+  }
+
+  componentDidMount() {
+    isSignedIn()
+      .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+      .catch(err => alert("An error occurred"));
+  }
+
+  render() {
+    const { checkedSignIn, signedIn } = this.state;
+
+    // If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
+    if (!checkedSignIn) {
+      return null;
+    }
+
+    const Layout = createRootNavigator(signedIn);
+    return <Layout />;
+  }
+}
